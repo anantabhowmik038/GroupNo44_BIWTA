e57555e502ec2a36c1d757a327866daf3214550a
anantabhowmik038 <anantabhowmik038@gmail.com>
2025-11-27 10:31:28 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.groupno44_biwta {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.groupno44_biwta to javafx.fxml;
+    exports com.example.groupno44_biwta;
+}

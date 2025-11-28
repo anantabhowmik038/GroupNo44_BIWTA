b2ff051312f529a70f17568d96de84967d67598c
anantabhowmik038 <anantabhowmik038@gmail.com>
2025-11-28 17:15:16 +0600

    first commit

---
+++ src/main/java/com/example/groupno44_biwta/Ananta/PassengerDashboardController.java
@@ -0,0 +1,30 @@
+package com.example.groupno44_biwta.Ananta;
+
+import javafx.event.ActionEvent;
+
+public class PassengerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleOnMyBookingsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleOnPassengerDashboardLogOutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleOnBookTicketsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleOnFeedbackAndSupportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleOnLiveFerryTrackerButton(ActionEvent actionEvent) {
+    }
+}
